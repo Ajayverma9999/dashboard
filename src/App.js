e0748cc2de19679fd53f419c8f900a3244d001ed
Parjapati...Pc <ajaygsss1@gmail.com>
2024-02-03 11:24:35 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,57 @@
-import React from 'react';
+// import React from 'react';
+// import Home from './Home';
+// import Login from './components/Login';
+// import { Route, Routes } from 'react-router-dom';
+
+
+
+
+// function App() {
+//   return (
+//     <div className="app-container">
+//       <Routes>
+//         <Route path="/" element={<Login/>} />
+//         <Route path="/home" element={<Home/>} />
+//        </Routes>
+
+
+//     </div>);
+// }
+
+// export default App;
+import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
-// import Dashboard from './components/Dashboard'
 import Contect from './components/Contact.jsx';
 import Grow from './components/Grow'
 import { Route, Routes } from 'react-router-dom';
-import Dashboard from './components/Dashboard.jsx';
 
-function App() {
+import Blog from './components/Blog.jsx';
+import Login from './components/Login.jsx';
+
+
+function Home() {
+
+
   return (
-    <div className="app-container">
+
+
+    <div className="Home-container">
       <Sidebar />
+
       <Routes>
-        <Route path="/home" element={<Dashboard/>} />
-        <Route path="/Contact" element={<Contect/>} />
-        <Route path="/GrowWithUS" element={<Grow/>} />
+          <Route path="/" element={<Login/>} />
+          {/* <Route path="/home" element={<Sidebar/>} /> */}
+          <Route path="/Contact" element={<Contect/>} />
+          <Route path="/GrowWithUS" element={<Grow/>} />
+          <Route path="/blog" element={<Blog/>} />
       </Routes>
-    </div>);
+    </div>
+
+  );
+
 }
 
-export default App;
+
+export default Home;
+
+
